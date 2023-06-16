fd43bfd97c4f7a072a22d52b97dcd08dbdaa0502
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-16 12:04:12 -0700

    betty style description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description : The program prints the contents of a char array using puts() function
+ *
+ * Return: 0 on a successful execution
+ */
 int main(void)
 {
 	/*This program prints the contents of a string using puts()*/
