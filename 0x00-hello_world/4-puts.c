5dfb4292d816fc196b4952c55ae5fae21fb05f13
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-16 11:57:12 -0700

    adding commment as per betty-doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
+	/*This program prints the contents of a string using puts()*/
+
 	char string[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(string);
