2b7e9f9bf6b56533d046e4a3c0e0ccf596c96c43
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-18 13:44:45 -0700

    put() new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,9 +11,10 @@ int main(void)
 {
 	/*This program prints the contents of a string using puts()*/
 
-	char string[] = "\"Programming is like building a multilingual puzzle\n";
+	char string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
+	puts('\n');
 
 	return (0);
 }
