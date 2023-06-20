a8cf28461566a4a75022c1e48b0ef801d5386e32
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-20 09:38:00 -0700

    check for character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for characters
+ * @c: takes an integer is as parameter
+ *
+ * Return: 1 if c is a letter else return 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 0-putchar.c: prints " _putchar" on the screen
 1-alphabet.c: prints the alphabet in lowercase
 2-print_alphabet_x10.c: prints the alphabet ten times
+3-islower.c: checks for lowercase characters
+4-isalpha.c: checks for characters
