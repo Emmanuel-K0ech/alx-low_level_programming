340ec5a3ff516cb4add3c27502ea53d57c6a46e8
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-22 06:20:17 -0700

    changed to for loop insted

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,15 +6,15 @@
  */
 void print_line(int n)
 {
-	do {
+	for (n = n; n > 0; n--)
+	{
 		if (n <= 0)
 		{
 			_putchar('\n');
 			break;
 		}
-
-		_putchar('_');
+		else
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
-		n--;
-	} while (n > 0);
-}
