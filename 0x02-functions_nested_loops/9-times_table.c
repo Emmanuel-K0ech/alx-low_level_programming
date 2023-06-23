c0bdf556fdde575c97a6796132e1b065927ca971
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-23 12:38:37 -0700

    using nested for loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,38 +5,14 @@
  */
 void times_table(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(0 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(1 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(2 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(3 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(4 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(5 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(6 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(7 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(8 * i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(9 * i);
-		_putchar('\n');
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d, ", i * j);
+		}
+		printf("\n");
 	}
 }
