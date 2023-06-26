8bee8a76a59cd92ecd70e3bd5de64b63b50c5ae5
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-26 08:56:12 -0700

    strcpy()

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies string from one locatin to another
+ * @dest: destination (locatin to  copy to)
+ * @src: location to copy from
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char s1[98];
+
+	s1 = *src;
+	dest = s1;
+
+	return (dest);
+}
