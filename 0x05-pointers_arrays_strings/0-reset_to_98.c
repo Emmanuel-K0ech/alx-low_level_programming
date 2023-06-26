36547d7d1917d19efeb65381005b05c8a88ea814
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-26 02:40:08 -0700

    assingning value to pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - assigns value 98 to the address given
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c: function that takes pointer to an integer as parameter and updates the value it points to to 98
