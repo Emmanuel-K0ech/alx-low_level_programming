049a138430c846e8828b07538e9e816c217935bb
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-06-29 15:40:34 -0700

    copying string with maximum limit of n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - copies a specified number of characters from one string
+ * to another
+ * @dest: pointer to the destinaton of the copied string
+ * @src: pointer to the string where characters will be copied
+ * @n: maximum number of characters to be copied from *src
+ *
+ * Return: pointer to the result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (dest != (dest + n))
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-strcat.c: implementation of string concatination
 1-strncat.c: concatenation of strings with a limit
+2-strncpy.c: copy a string with a limit
