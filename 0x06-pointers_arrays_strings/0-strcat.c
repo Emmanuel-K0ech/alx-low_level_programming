3570199744c56708cd7cc5dfe5f72948adf249bd
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-01 04:45:46 -0700

    adding the null byte to the concatenated string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,20 +4,26 @@
  * @dest: pointer to the string to append to
  * @src: pointer to the string to append
  *
- * Return: pointer to dest (concateneated string)
+ * Return: dest (concateneated string)
  */
 char *_strcat(char *dest, char *src)
 {
+	/* moving to the end of the destination string */
 	while (*dest != '\0')
 	{
 		dest++;
 	}
 
+	/* copying the src strng to dest string, overwriting null byte */
 	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
 		dest++;
 	}
+
+	/* add a null byte at the end of concatenated string */
+	*dest = '\0';
+
 	return (dest);
 }
