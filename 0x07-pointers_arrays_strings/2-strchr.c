77ca09ed84a19d7bc4e1d0d9fe720ed4d8980d87
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-03 14:15:43 -0700

    using array notation instead

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,22 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr - locates a character in a string
+ * @s: string to search throught
  * @c: character to be located
  *
  * Return: first occurrence of the character or NULL
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		else
-		{
-			return ('\0');
-		}
-		s++;
 	}
+	return ('\0');
 }
-			
