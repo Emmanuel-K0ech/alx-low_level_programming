8b0363e30791eaf599bcd624c53516975b1bf73d
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-03 15:22:31 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string to check across
+ * @accept: character being counted in bytes
+ *
+ * Return: unsigned int representing the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int flag = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+				flag++;
+		}
+	}
+	return (flag);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,4 @@
 0-memset.c: replaces n bytes with a character defined by the user
 1-memcpy: copies n bytes from one string and copies it to another
+2-strchr.c: locates a character in a string
+3-strspn.c: gets the length of a prefix substring
