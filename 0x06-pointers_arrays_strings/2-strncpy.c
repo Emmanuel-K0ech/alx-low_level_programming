5409b5a046a7dac853c45050be4000ba03cc02d2
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-02 20:55:26 -0700

    strncpy implementation

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strncpy - copies a specified number of characters from one string
  * to another
@@ -6,20 +7,48 @@
  * @src: pointer to the string where characters will be copied
  * @n: maximum number of characters to be copied from *src
  *
- * Return: pointer to the result
+ * Return: pointer to dest(result)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (*dest != '\0')
-	{
-		dest++;
-	}
+	char *result;
+	int len_src;
+	int flag;
 
-	while (dest != (dest + n))
+	/* finding length of string to compare with n */
+	len_src = strlen(src);
+	flag = 0;
+	result = dest;
+
+	/* seperate n < strlen(src) and n > strlen(src) scenarios */
+	if (n > len_src)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		while (*src != '\0')
+		{
+			*dest = *src;
+			src++;
+			dest++;
+			flag++;
+		}
+
+		while (flag < n)
+		{
+			*dest = '\0';
+			dest++;
+			flag++;
+		}
 	}
-	return (dest);
+	else
+	{
+		while (flag < n)
+		{
+			*dest = *src;
+			src++;
+			dest++;
+			flag++;
+		}
+
+		*dest = '\0';
+	}
+	return (result);
 }
