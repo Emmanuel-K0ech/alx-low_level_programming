66030e64c9d37d7b3500dd6ee988e7cf07ed415b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-04 05:55:20 -0700

    using two flags

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,6 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 	unsigned int flag = 0;
+	unsigned int flag2 = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -18,6 +19,11 @@ unsigned int _strspn(char *s, char *accept)
 			if (accept[j] == s[i])
 				flag++;
 		}
+
+		if (flag2 < flag)
+			flag2++;
+		else
+			break;
 	}
-	return (flag);
+	return (flag2);
 }
