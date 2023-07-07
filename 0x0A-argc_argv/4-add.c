2fedcbcff7a9b7a6d1ddbc371655083f275ef54e
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-07 06:24:16 -0700

    adding positve values passed as arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguent vector
+ *
+ * Description: adds all positive integers passed as arguments to the prgram
+ *
+ * Return: 0 on success 1 on Error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc <= 1)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) ==  0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
