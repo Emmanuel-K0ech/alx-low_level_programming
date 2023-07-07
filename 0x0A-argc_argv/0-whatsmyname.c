8fbeb8df2bf7bde95ff09ccbbad6871d22d0b3d7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-07 04:21:37 -0700

    renaming directory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: prints the name of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
