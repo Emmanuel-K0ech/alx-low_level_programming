b5a220ec1f942144ee0fd141c63634fd18275c75
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-11 02:45:22 -0700

    handling malloc return

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,6 +17,8 @@ char *_strdup(char *str)
 
 	size = strlen(str);
 	ptr = malloc((sizeof(char) * size) + 1);
+	if (ptr == NULL)
+		return (NULL);
 	while (i < ((sizeof(char) * size) + 1))
 	{
 		ptr[i] = str[i];
