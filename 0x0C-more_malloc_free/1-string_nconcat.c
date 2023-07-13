90cd63a4afdb8397bb620de53d6d402833555613
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-13 02:26:07 -0700

    string concatenation to dynamically allocated memory

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: equivalent to dest from strncpy()
+ * @s2: equivalent to src
+ * @n: the number of bytes being copies from src
+ *
+ * Return: pointer to new string on success or NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *result;
+	char *ptr;
+	unsigned int total_len = 0;
+
+	total_len = strlen(s1) + strlen(s2) - n;
+	ptr = malloc((sizeof(char) * total_len) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	result = ptr;
+	strcat(ptr, s1);
+	strncat(ptr, s2, n);
+	return (result);
+}
