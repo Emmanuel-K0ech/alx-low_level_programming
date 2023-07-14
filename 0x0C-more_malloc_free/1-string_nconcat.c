355797b7a058de7988ad0ed1b06f8e9dcce9aa66
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-14 05:10:12 -0700

    used = instead of ==

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int total_len = 0;
 
-	if (s1 = NULL && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		retrun (NULL);
 	if (s1 == NULL)
 		s1 = " ";
