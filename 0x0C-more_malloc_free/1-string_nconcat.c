e6f12b58ae4f339d06a7266d85938f74fe563153
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-14 05:32:24 -0700

    assinging \0 IF strings passed are NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int total_len = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = '\0';
 	if (s2 == NULL)
