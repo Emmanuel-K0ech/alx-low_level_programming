3fc8f5240364c28b05ceea5eb90118879d7487f7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-14 06:33:42 -0700

    multiplying two numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: multiplication of 2 numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	long unsigned int i, j;
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < 2; i++)
+	{
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+
+	return (0);
+}
