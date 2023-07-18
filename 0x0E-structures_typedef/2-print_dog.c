8d8533239951c598095605faa8a325f85457c399
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-18 09:46:42 -0700

    formating IF statement logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,17 +9,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
-		printf("\n");
 		exit(0);
-	}
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
-		printf("Age: (nil)\n");
-	else
 		printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
