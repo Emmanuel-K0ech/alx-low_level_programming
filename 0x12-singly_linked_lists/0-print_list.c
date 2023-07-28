78d6a51ab008f4ec69514038b7e2214494c30224
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-27 18:41:09 -0700

    adding condition IF str == NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,18 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			i++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
+		}
 	}
 	return (i);
 }
