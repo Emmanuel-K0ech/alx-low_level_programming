24d9ee0314cbde87315758a05f366bf85af46ee7
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-07-28 21:30:54 -0700

    constructor function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void hare(void) __attribute__((constructor));
+/**
+ * hare - prints to the screen
+ *
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!
+			\n");
+}
