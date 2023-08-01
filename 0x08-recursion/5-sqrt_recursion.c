1fcd61a458a4beeb3cba21c36e896a40ce82a9fd
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-08-01 13:09:59 -0700

    finding the squaroot of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * sqroot - find natural sqrt of a number
+ * @n: integer
+ *
+ * Return: integer
+ */
+int sqroot(int i, int j)
+{
+	if (i * i == j)
+		return (i);
+	else if (i * i > j)
+		return (-1);
+	else
+		return (sqroot(i + 1, j));
+}
+/**
+ * _sqrt_recursion - find natural sqrt of a number
+ * @n: int
+ *
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+	
+	return (sqroot(i, n));
+}
