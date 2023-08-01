13565fe02c9922d48ea830374995b1eea08701f9
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-08-01 14:12:46 -0700

    no else statements

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,7 @@ void print_all(const char * const format, ...)
 				str = va_arg(list, char*);
 				if (str == NULL)
 					printf("(nil)");
-				else
+				if (str != NULL)
 					printf("%s", str);
 				break;
 			default:
