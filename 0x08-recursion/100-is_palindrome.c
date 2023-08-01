1c7203dcdfc590dc0bca8294a57cfe05d81edcf5
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-08-01 13:47:52 -0700

    advanced tasks

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_palindrome - find if  a string is  palindrome
+ * @s: string to check
+ *
+ * Return: 1 for palindrome else 0
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+
+	if (*s == i)
+		return (0);
+	return (1);
+}
