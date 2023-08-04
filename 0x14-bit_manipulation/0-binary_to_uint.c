933ce30042544ed949e0dfb61f466992c083df6d
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-08-04 10:09:26 -0700

    using << operator instead of pow()

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include <string.h>
-#include <math.h>
 #include <stdlib.h>
 /**
  * binary_to_uint - converts binary to unsigned int
@@ -9,17 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	unsigned int result = 0;
 	unsigned int len = 0;
+	unsigned int temp = 0;
 
 	len = strlen(b);
+	temp = len - 1;
 	if (b == NULL)
 		return (0);
-	while (b[i] != '\0')
+	for (i = 0; i < len; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
-			result += pow(2, (len - i)) * b[i];
+			result += (1 << (temp - i)) * (b[i] - '0');
 		else
 			return (0);
 	}
