447a6daec4eca5f1fa35d34e6dc6afd9a87e4bd1
General Manu <kipchumbakoech1107@gmail.com>
2023-09-25 16:38:12 +0300

    file handling for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	len = strlen(text_content);
 	buffer = malloc((sizeof(char) * len) + 1);
 	if (buffer == NULL)
