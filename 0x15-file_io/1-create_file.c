9773d7d46ac3304d7fa766339f335ee0c93fa549
General Manu <kipchumbakoech1107@gmail.com>
2023-09-25 16:12:41 +0300

    refactoring code for task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd1 = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd1 = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd1 == -1)
 		return (-1);
 	fd2 = open(text_content, O_RDONLY);
