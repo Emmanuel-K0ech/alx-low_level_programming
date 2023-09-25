bf2a9ee561df2938f430f09521e4ac15263428d4
General Manu <kipchumbakoech1107@gmail.com>
2023-09-25 16:30:35 +0300

    task 1 refactored

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,38 +11,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd1, fd2, len = 0;
-	ssize_t rd_count, wr_count = 0;
+	int fd, len = 0;
+	ssize_t wr_count = 0;
 	char *buffer;
 
 	if (filename == NULL)
 		return (-1);
-	fd1 = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd1 == -1)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	fd2 = open(text_content, O_RDONLY);
-	if (fd2 == -1)
-	{
-		close(fd1);
-		return (-1);
-	}
 	len = strlen(text_content);
 	buffer = malloc((sizeof(char) * len) + 1);
 	if (buffer == NULL)
 		return (-1);
-	rd_count = read(fd2, buffer, len);
-	if (rd_count == -1)
-	{
-		free(buffer);
-		return (-1);
-	}
-	wr_count = write(fd1, buffer, len);
+	strcpy(buffer, text_content);
+	wr_count = write(fd, buffer, len);
 	if (wr_count == -1)
-	{
-		free(buffer);
 		return (-1);
-	}
-	close(fd1);
-	close(fd2);
+	close(fd);
 	return (1);
 }
