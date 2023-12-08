b2b8d291fcd759026e4469fd39873074cdb6cf5b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-08 02:16:29 -0800

    header file update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,6 +17,10 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+/* Header files */
+#include <stdio.h>
+#include <stdlib.h>
+
 /* Functions */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
