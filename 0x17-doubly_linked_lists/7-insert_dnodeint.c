e56e93fcb3236a129126afeba45e98c0ed681f87
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-09 13:17:30 -0800

    correcting logic

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,11 +23,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->n = n;
 			new_node->next = temp;
 			new_node->prev = temp->prev;
-			(temp->prev)->next = new_node;
+			if (temp->prev != NULL)
+				(temp->prev)->next = new_node;
+			temp->prev = new_node;
 			return (new_node);
 		}
 		temp = temp->next;
 		count++;
 	}
+	free(new_node);
 	return (NULL);
 }
