9fcbc0b7419b3d5fc2ea42e416a3eba6b87fefa4
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-09 13:26:37 -0800

    correct task 7 logic: scenario if idx == 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,11 +16,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 	while (temp != NULL)
 	{
 		if (count == idx)
 		{
-			new_node->n = n;
 			new_node->next = temp;
 			new_node->prev = temp->prev;
 			if (temp->prev != NULL)
