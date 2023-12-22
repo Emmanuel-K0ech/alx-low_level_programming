098d215e31422c95b13b5404e8e0965a6d54e284
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-22 13:57:00 -0800

    fixing betty error

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,7 +19,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
-		if(strcmp(temp->key, key) == 0)
+		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
 		temp = temp->next;
 	}
