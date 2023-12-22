a0eef6f44009fe50cd87e4258ca336517ed73d83
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-22 14:42:02 -0800

    taks 5: hash table print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints contents of a hash table
+ * @ht: hash table to print
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	int flag = 0;
+
+	if (ht == NULL)
+		exit(1);
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
