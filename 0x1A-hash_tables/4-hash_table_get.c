37c020cc4da4ec0ed4a6a94b6d23ed35e660e300
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-22 13:55:44 -0800

    correction for task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
+	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	return (ht->array[index]->value);
+	if ((ht->array[index]) == NULL)
+		return (NULL);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if(strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
 }
