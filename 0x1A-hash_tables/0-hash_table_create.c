bddbc6c791dc6b5bcb61e6aa229ea22472476d9d
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2023-12-22 12:12:58 -0800

    changing logic

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_array;
 
-	new_array = malloc(sizeof(hash_node_t *) * size);
+	new_array = malloc(sizeof(hash_table_t));
 	if (new_array == NULL)
 		exit(EXIT_FAILURE);
 	new_array->size = size;
+	new_array->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_array->array == NULL)
+		exit(EXIT_FAILURE);
 	return (new_array);
 }
