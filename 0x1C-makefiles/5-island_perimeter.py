fa54cfdb975b6dfcb45d1041f83a6abf3cb5f345
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-05-14 14:25:09 -0700

    Task 5: Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Definition of an island perimeter measuring function"""
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of the grid
+    The grid, 0 for water and 1 for land
+
+    Args:
+        grid (list): A list of lists of integrs representing the island
+    Returns: The perimeter
+    """
+    height = len(grid)
+    width = len(grid[0])
+    edge = 0
+    size = 0
+    perimeter = 0
+
+    for i in rage(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edge += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edge += 1
+    perimeter = (size * 4)-(edge * 2)
+    return perimeter
